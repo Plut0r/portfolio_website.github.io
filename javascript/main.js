882cd5151d8115f52e5dad71fb5f538ae1040201
Ogunola Zainab Ololade <ogunolaololade@gmail.com>
2022-06-07 13:03:20 +0100

    Update main.js
    Made adjustments to js file

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,10 @@
 function bigImg(e) {
-e.style.width = 400px;
-e.style.height = 400px;
+e.style.width = "400px";
 }
 
 function normalImg(e) {
-e.style.width = 350px;
-e.style.height = 350px;
+e.style.width = "350px";
+e.style.height = "350px";
 }
 
-const image = document.getElementsByClassName('image');
-image.addEventListener('mouseenter', bigImg);
-image.addEventListener('mouseleave', normalImg);
+
